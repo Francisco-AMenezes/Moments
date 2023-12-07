8d20f8118286b2a1ea9ffbd50d2a16981b986545
Francisco Menezes <franciscoalvesmenezes90@gmail.com>
2023-12-07 16:19:10 -0300

    Add messages component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 import { MomentFormComponent } from './components/moment-form/moment-form.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     HomeComponent,
     NewMomentComponent,
     MomentFormComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
