405a74f80de404d83351f25bcb00c3ead250ef21
Francisco Menezes <franciscoalvesmenezes90@gmail.com>
2023-12-11 14:38:13 -0300

    Pré add ionic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
+
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  {path: 'moments/new', component: NewMomentComponent}
+  { path: 'moments/new', component: NewMomentComponent },
+
 ];
 
 @NgModule({
